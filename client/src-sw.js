986eb01b9bbdcdf1703e799203aa1a98b8ca34d0
honguyen00 <lehonguyen00@gmail.com>
2024-01-04 17:32:45 +1100

    implement update and get data from idb, implement service worker for asset cache, implement module, inject manifest & webpackmanifest in webpack config

---
+++ client/src-sw.js
@@ -0,0 +1,46 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/manifest.json', '/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+registerRoute(({ request }) => {return (request.destination === 'image' || request.destination === 'style' || request.destination === 'script')}, 
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ]
+  })
+);
+
+self.addEventListener('fetch', (e) =>
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)))
+);
